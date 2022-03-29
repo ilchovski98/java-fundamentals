4437ee3fe716efd8aee0940203c346bc83e41d41
ilchovski98 <ilchovski98@gmail.com>
2022-03-29 22:35:35 +0300

    Refactor Mortage code from functional to using classes (alone)

---
+++ oop-design-mortage/src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+        var mortage = new Mortage();
+        mortage.printMortgage();
+        mortage.printPaymentSchedule();
+    }
+}
