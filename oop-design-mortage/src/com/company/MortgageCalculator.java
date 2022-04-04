c2e63482089bca0a424ae142f1bf12b4dcc266dc
ilchovski98 <ilchovski98@gmail.com>
2022-04-04 21:56:37 +0300

    Refactor Mortgage Calculator and Mortgage Report

--- oop-design-mortage/src/com/company/MortgageCalculator.java
+++ oop-design-mortage/src/com/company/MortgageCalculator.java
@@ -1,8 +1,10 @@
 package com.company;
 
+import java.text.NumberFormat;
+
 public class MortgageCalculator {
-    public final static byte MONTHS_IN_YEAR = 12;
-    public final static byte PERCENT = 100;
+    private final static byte MONTHS_IN_YEAR = 12;
+    private final static byte PERCENT = 100;
     private int principal;
     private float annualInterest;
     private int years;
@@ -16,8 +18,8 @@ public class MortgageCalculator {
     public double calculateBalance(
             short numberOfPaymentsMade
     ) {
-        float monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
-        float numberOfPayments = years * MONTHS_IN_YEAR;
+        float monthlyInterest = getMonthlyInterest();
+        float numberOfPayments = getNumberOfPayments();
 
         double balance = principal
                 * (Math.pow(1 + monthlyInterest, numberOfPayments) - Math.pow(1 + monthlyInterest, numberOfPaymentsMade))
@@ -27,8 +29,8 @@ public class MortgageCalculator {
     }
 
     public double calculateMortgage() {
-        float monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
-        float numberOfPayments = years * MONTHS_IN_YEAR;
+        float monthlyInterest = getMonthlyInterest();
+        float numberOfPayments = getNumberOfPayments();
 
         double mortgage = principal
                 * (monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments))
@@ -37,7 +39,19 @@ public class MortgageCalculator {
         return mortgage;
     }
 
-    public int getYears() {
-        return years;
+    public double[] getRemainingBalances() {
+        var balances = new double[getNumberOfPayments()];
+        for (short month = 1; month <= balances.length; month++)
+            balances[month - 1] = calculateBalance(month);
+
+        return balances;
+    }
+
+    private int getNumberOfPayments() {
+        return years * MONTHS_IN_YEAR;
+    }
+
+    private float getMonthlyInterest() {
+        return annualInterest / PERCENT / MONTHS_IN_YEAR;
     }
 }
--- oop-design-mortage/src/com/company/MortgageReport.java
+++ oop-design-mortage/src/com/company/MortgageReport.java
@@ -4,10 +4,12 @@ import java.text.NumberFormat;
 
 public class MortgageReport {
 
+    private final NumberFormat currency;
     private MortgageCalculator calculator;
 
     public MortgageReport(MortgageCalculator calculator) {
         this.calculator = calculator;
+        currency = NumberFormat.getCurrencyInstance();
     }
 
     public void printPaymentSchedule() {
@@ -15,10 +17,9 @@ public class MortgageReport {
         System.out.println("PAYMENT SCHEDULE");
         System.out.println("----------------");
 
-        for (short month = 1; month <= calculator.getYears() * MortgageCalculator.MONTHS_IN_YEAR; month++) {
-            double balance = calculator.calculateBalance(month);
-            System.out.println(NumberFormat.getCurrencyInstance().format(balance));
-        }
+        for (double balance : calculator.getRemainingBalances())
+            System.out.println(currency.format(balance));
+
     }
 
     public void printMortgage() {
