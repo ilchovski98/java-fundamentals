84f88bc91a968901cd5bac3a3f5d1f51c28a0d21
ilchovski98 <ilchovski98@gmail.com>
2022-04-18 13:51:43 +0300

    Editing textbox downcasting example

--- inheritance/src/com/company/Main.java
+++ inheritance/src/com/company/Main.java
@@ -1,14 +1,19 @@
 package com.company;
 
+import org.w3c.dom.Text;
+
 public class Main {
     public static void main(String[] args) {
         var control = new UIControl(true);
         var textBox = new TextBox();
-        show(control);
+        show(textBox);
     }
 
     public static void show(UIControl control) {
-        var textBox = (TextBox)control;
+        if (control instanceof TextBox) {
+            var textBox = (TextBox)control;
+            textBox.setText("Hello World!");
+        }
         System.out.println(control);
     }
 }
