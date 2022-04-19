17d55a70c7e95d0473b32319c31ecef8d49629e5
ilchovski98 <ilchovski98@gmail.com>
2022-04-20 01:47:37 +0300

    Method injection

--- interfaces/src/com/company/Main.java
+++ interfaces/src/com/company/Main.java
@@ -3,10 +3,8 @@ package com.company;
 public class Main {
     public static void main(String[] args) {
         var calculator = new TaxCalculator2018(100_000);
-        var taxReport = new TaxReport(calculator);
-        taxReport.show();
-
-        taxReport.setCalculator(new TaxCalculator2019());
-        taxReport.show();
+        var taxReport = new TaxReport();
+        taxReport.show(calculator);
+        taxReport.show(new TaxCalculator2019());
     }
 }
--- interfaces/src/com/company/TaxReport.java
+++ interfaces/src/com/company/TaxReport.java
@@ -3,11 +3,11 @@ package com.company;
 public class TaxReport {
     private TaxCalculator calculator;
 
-    public TaxReport(TaxCalculator calculator) {
-        this.calculator = calculator;
-    }
+//    public TaxReport(TaxCalculator calculator) {
+//        this.calculator = calculator;
+//    }
 
-    public void show() {
+    public void show(TaxCalculator calculator) {
         var calc = calculator.calculateTax();
         System.out.println(calc);
     }
